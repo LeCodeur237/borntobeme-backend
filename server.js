3fcc775cb77df33b97b4cf52acd34c60790aa2e8
LeCodeur237 <erastedzou@gmail.com>
2025-06-12 18:04:34 +0200

    v12-06-2025

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // Connect to MongoDB
