44879e9632ad35c483fa7f98a6ea2ba46f090783
LeCodeur237 <erastedzou@gmail.com>
2025-06-15 16:46:18 +0200

    vg

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:8080",
       "http://localhost:8081",
+      "http://localhost:5174"
     ],
     optionsSuccessStatus: 200,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
